df05127fb68da18ec365c0d0452c06ee76622418
vishank91 <vishankchauhan@gmail.com>
2026-06-08 14:04:28 +0530

    08-06-2026

--- index.js
+++ index.js
@@ -3,25 +3,26 @@ const cors = require("cors")
 const path = require("path")
 
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        const allowedOrigins = ["http://localhost:5173","http://localhost:8000","https://heritage-ally-server.onrender.com/"];
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         const allowedOrigins = ["http://localhost:5173","http://localhost:8000","https://heritage-ally-server.onrender.com/"];
 
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("You Are not Authorized to Access This API"));
-        }
-    },
-    credentials: true,
-}; 
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error("You Are not Authorized to Access This API"));
+//         }
+//     },
+//     credentials: true,
+// }; 
 
 require("dotenv").config()
 require("./db-connect")
 
 const Router = require("./routes/index")
 const app = express()
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
+app.use(cors())
 
 app.use(express.json())
 app.use("/public", express.static("./public"))
